0bbed4ecd16f6c504cee6b5b2edad84a20c99d5e
David Pallmann <dpallmann@hotmail.com>
2022-04-24 18:44:44 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using Amazon;
+using Amazon.CostExplorer;
+using Amazon.CostExplorer.Model;
+
+// Get date range from the command line
+
+if (args.Length < 2)
+{
+    Console.WriteLine("Usage: dotnet run -- [start-yyyy-mm-dd] [end-yyyy-mm-dd]");
+}
+
+var startDate = args[0];
+var endDate = args[1];
+
+// Query the Cost Explorer API
+
+var client = new AmazonCostExplorerClient();
+var request = new GetCostAndUsageRequest()
+{
+    Granularity = "MONTHLY",
+    GroupBy = { new GroupDefinition() { Key = "SERVICE", Type = GroupDefinitionType.DIMENSION } },
+    TimePeriod = new DateInterval()
+    {
+        Start = startDate,
+        End = endDate
+    },
+    Metrics = { "BlendedCost" }
+};
+
+var response = await client.GetCostAndUsageAsync(request);
+
+// Display results
+
+foreach (var result in response.ResultsByTime)
+{
+    Console.WriteLine($"{result.TimePeriod.Start}-{result.TimePeriod.End}");
+    foreach(var group in result.Groups)
+    {
+        foreach(var key in group.Keys)
+        {
+            var amount = group.Metrics.FirstOrDefault().Value.Amount;
+            if (amount != "0")
+            {
+                Console.WriteLine($"    {key} {group.Metrics.FirstOrDefault().Value.Amount} {group.Metrics.FirstOrDefault().Value.Unit}  ");
+            }
+        }
+    }
+    Console.WriteLine();
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Hello, Cost Explorer!
+
+This is the code project for the [Hello, Cost Explorer!](https://davidpallmann.hashnode.dev/hello-cost-explorer) blog post. 
+
+This episode: AWS Cost Explorer and cost analysis. In this Hello, Cloud blog series, we're covering the basics of AWS cloud services for newcomers who are .NET developers. If you love C# but are new to AWS, or to this particular service, this should give you a jumpstart.
+
+In this post we'll introduce AWS Cost Explorer and use it in a "Hello, Cloud" .NET program to perform query AWS charges. We'll do this step-by-step, making no assumptions other than familiarity with C# and Visual Studio. We're using Visual Studio 2022 and .NET 
+
+## Our Hello, Cost Explorer Project
+
+We will first get familiar with Cost Explorer in the AWS console. Then, we'll query it from the AWS CLI. Finally, we'll write a .NET program to get cost and usage information from the Cost Explorer API.
+
+See the blog post for the tutorial to create this project and run it on AWS.
